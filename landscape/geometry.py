8b7a4e998111c279c89f99c522f8fc65dd5b3bad
Ted Tang <s23tang@gmail.com>
2019-01-22 09:09:43 -0500

    Added classes for computing geometry + unit tests

---
+++ landscape/geometry.py
@@ -0,0 +1,76 @@
+import os
+import random
+from dataclasses import dataclass
+from itertools import tee
+
+
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    @staticmethod
+    def midpoint_x(point1, point2):
+        return (point1.x + point2.x)/2
+
+    @staticmethod
+    def midpoint_y(point1, point2):
+        return (point1.y + point2.y)/2
+
+    @staticmethod
+    def midpoint(point1, point2):
+        return Point(
+            Point.midpoint_x(point1, point2),
+            Point.midpoint_y(point1, point2)
+        )
+
+    @staticmethod
+    def displaced_midpoint(point1, point2, displace_factor):
+        midpoint = Point.midpoint(point1, point2)
+        midpoint.displace(displace_factor)
+        return midpoint
+
+    def displace(self, displace_factor):
+        self.y += random.choice([-displace_factor, displace_factor])
+
+
+class XOrderedSegment:
+    def __init__(self):
+        self.points = []
+
+    @property
+    def pairwise_points(self):
+        p1, p2 = tee(self.points)
+        next(p2, None)
+        return list(zip(p1, p2))
+
+    def add(self, *points):
+        for point in points:
+            self._add_point(point)
+
+    def _add_point(self, point):
+        low, high = 0, len(self.points)
+        while low < high:
+            mid = (low+high)//2
+            if point.x < self.points[mid].x:
+                high = mid
+            else:
+                low = mid + 1
+        self.points.insert(low, point)
+
+    def iterative_displacement(self, displace_factor, roughness, iterations):
+        for _ in range(iterations):
+            self.displace_midpoints(displace_factor)
+            displace_factor = self._roughen_displace_factor(displace_factor, roughness)
+
+    def displace_midpoints(self, displace_factor):
+        for p1, p2 in self.pairwise_points:
+            displaced_midpoint = Point.displaced_midpoint(p1, p2, displace_factor)
+            self.add(displaced_midpoint)
+
+    def _roughen_displace_factor(self, displace_factor, roughness):
+        return displace_factor * pow(2, -roughness)
+
---
+++ tests/geometry_test.py
@@ -0,0 +1,136 @@
+from PIL import Image, ImageDraw
+from landscape.geometry import Point, XOrderedSegment
+
+
+class TestPoint:
+    def test_point_create(self):
+        p = Point(1,2)
+        assert p.x == 1
+        assert p.y == 2
+
+    def test_midpoint_x_calculated_correctly(self):
+        mid_x_even = Point.midpoint_x(Point(-10,5), Point(8, -6))
+        mid_x_rounded = Point.midpoint_x(Point(1,1), Point(2,10))
+        assert mid_x_even == -1.0
+        assert mid_x_rounded == 1.5
+
+    def test_midpoint_y_calculated_correctly(self):
+        mid_y_even = Point.midpoint_y(Point(-10,5), Point(8, 5))
+        mid_y_rounded = Point.midpoint_y(Point(1,5), Point(2,-6))
+        assert mid_y_even == 5.0
+        assert mid_y_rounded == -0.5
+
+    def test_y_displacement(self):
+        p = Point(1,3)
+        p.displace(5)
+        assert p.y in (-2.0, 8.0)
+        p.displace(-2.5)
+        assert p.y in (-4.5, 0.5, 5.5, 10.5)
+
+    def test_displaced_midpoint(self):
+        p1, p2 = Point(1,2), Point(-10,9)
+        p3 = Point.displaced_midpoint(p1, p2, 5)
+        assert p3.x == -4.5
+        assert p3.y in (0.5, 10.5)
+
+    def test_comparison(self):
+        p1, p2, p3 = Point(1,2), Point(2,3), Point(1,2)
+        assert p1 == p3
+        assert p1 != p2
+        assert p2 != p3
+
+
+class TestXOrderedSegment:
+    def test_add_point_is_ordered_ascending(self):
+        s = XOrderedSegment()
+        p1 = Point(1,2)
+        p2 = Point(-10,5)
+        p3 = Point(50,-9)
+        s._add_point(p1)
+        assert s.points == [p1]
+        s._add_point(p2)
+        assert s.points == [p2, p1]
+        s._add_point(p3)
+        assert s.points == [p2, p1, p3]
+
+    def test_add_multiple_points(self):
+        s = XOrderedSegment()
+        p1 = Point(10,10)
+        p2 = Point(15,10)
+        p3 = Point(-90,-50)
+        s.add(p1, p2, p3)
+        assert s.points == [p3, p1, p2]
+
+        p4 = Point(5,5)
+        s.add(p4)
+        assert s.points == [p3, p4, p1, p2]
+
+    def test_pairwise_points(self):
+        s = XOrderedSegment()
+        assert s.points == []
+        assert s.pairwise_points == []
+
+        p1 = Point(1,1)
+        s.add(p1)
+        assert s.points == [p1]
+        assert s.pairwise_points == []
+
+        p2 = Point(-3,100)
+        s.add(p2)
+        assert s.points == [p2, p1]
+        assert s.pairwise_points == [(p2, p1)]
+
+        p3 = Point(10, 0)
+        s.add(p3)
+        assert s.points == [p2, p1, p3]
+        assert s.pairwise_points == [(p2, p1), (p1, p3)]
+
+        p4 = Point(-90, 1)
+        s.add(p4)
+        assert s.points == [p4, p2, p1, p3]
+        assert s.pairwise_points == [(p4, p2), (p2, p1), (p1, p3)]
+
+    def test_displace_midpoints(self):
+        s = XOrderedSegment()
+        s.displace_midpoints(5)
+        assert s.points == []
+
+        p1 = Point(5,0)
+        s.add(p1)
+        s.displace_midpoints(5)
+        assert s.points == [p1]
+
+        p2 = Point(-1,1)
+        s.add(p2)
+        s.displace_midpoints(5)
+        assert len(s.points) == 3
+        assert s.points[0] == p2 and s.points[2] == p1
+        assert s.points[1] in (Point(2,5.5), Point(2,-4.5))
+
+        s.displace_midpoints(1)
+        assert len(s.points) == 5
+        assert s.points[0] == p2 and s.points[4] == p1
+        assert s.points[2] in (Point(2,5.5), Point(2,-4.5))
+        assert s.points[1].x == 0.5
+        assert s.points[3].x == 3.5
+
+    def test_iterative_displacement(self):
+        s = XOrderedSegment()
+        factor, rough, iters = 5, 2, 2
+        s.iterative_displacement(factor, rough, iters)
+        assert s.points == []
+
+        p1 = Point(1,2)
+        s.add(p1)
+        s.iterative_displacement(factor, rough, iters)
+        assert s.points == [p1]
+
+        p2 = Point(3,4)
+        s.add(p2)
+        s.iterative_displacement(factor, rough, iters)
+        assert len(s.points) == 5
+        assert s.points[0] == p1 and s.points[4] == p2
+        assert s.points[2] in (Point(2,8), Point(2,-2))
+        assert s.points[1].x == 1.5
+        assert s.points[3].x == 2.5
+
